0b092969eb46240632ff2f3b513262e28e5ea0cf
Carl Ehrett <cehrett@clemson.edu>
2024-04-02 13:42:51 -0400

    Improved import structure

--- full_pipeline.py
+++ full_pipeline.py
@@ -1,28 +1,20 @@
-from extract_frames import process_and_save_tweets
-from get_frame_embeddings import get_embeddings
-from cluster_frames import cluster_embeddings
-from find_suspicious_frame_clusters import run_analysis
-from find_suspicious_account_clusters import bayesian_clustering
-from visualize_frame_clusters_across_time import visualize_frame_cluster_across_time
 
 import os
 
 # Config:
 # User-defined inputs
-data_path = os.path.join('/', 'zfs', 'disinfo', 'narratives', 'TANCOL', 'TANCOL_Complete Coded Data.csv')
-output_dir = os.path.join('/', 'zfs', 'disinfo', 'narratives', 'TANCOL', 'frame_extraction_outputs')
-text_col = 'full_text'
+data_path = os.path.join('/', 'zfs', 'disinfo', 'hunts', 'Africa_Elections', 'EconomicFreedomFightersMarch25.csv')
+output_dir = os.path.join('/', 'zfs', 'disinfo', 'hunts', 'Africa_Elections', 'frame_extraction_analysis', 'outputs', 'EconomicFreedomFightersMarch25')
+text_col = 'Message'
 api_key_loc = os.path.join('/', 'home', 'cehrett', '.apikeys', 'openai_api_key.txt')
-id_col = 'id'
-prompt_txt_loc = os.path.join('/', 'zfs', 'disinfo', 'hunts', 'navalny', 'carl', 'system_prompt.txt')
+id_col = 'UniversalMessageId'
+prompt_txt_loc = os.path.join('/', 'zfs', 'disinfo', 'hunts', 'Africa_Elections', 'frame_extraction_analysis', 'system_prompt.txt')
 username='cehrett'
-query_theories = ['TANCOL is real', 'We must defeat TANCOL', 'TANCOL is a threat to us']
-time_col = 'created_at'
 
 # Optional user-defined inputs
 # Frame-clustering settings
-umap_dim = 25
-min_cluster_size = 20
+umap_dim = 50
+min_cluster_size = 10
 
 # Suspicious frame-cluster identification settings
 flags = []
@@ -41,15 +33,24 @@ num_particles = 1
 dropout_rate = 0.3
 cholesky_rank = 2
 
+# Time series visualization settings
+num_bins = 12
+query_theories = ['There is corruption in the African National Congress', 
+                  'The Zondo Commission findings are a threat to the ANC', 
+                  'The Judicial Commission of Inquiry into Allegations of State Capture must be taken seriously']
+time_col = 'CreatedTime'
+round_to_nearest = 'H'
+
 # Which parts of the pipeline to run
 do_process_and_save_tweets = False
 do_get_embeddings = False
-do_cluster_embeddings = True
+do_cluster_embeddings = False
 do_run_analysis = False
 do_bayesian_clustering = False
 do_visualize_frame_cluster_across_time = True
 
 if do_process_and_save_tweets:
+    from extract_frames import process_and_save_tweets
     process_and_save_tweets(data_path,
                            results_dir=output_dir,
                            text_col=text_col,
@@ -58,11 +59,13 @@ if do_process_and_save_tweets:
                           )
 
 if do_get_embeddings:
+    from get_frame_embeddings import get_embeddings
     get_embeddings(embeddings_path=os.path.join(output_dir, 'frame_embeddings.json'),
                   frames_path=os.path.join(output_dir, 'frame_extraction_results.csv'),
                   api_key_loc=api_key_loc)
 
 if do_cluster_embeddings:
+    from cluster_frames import cluster_embeddings
     cluster_embeddings(frames_path=os.path.join(output_dir, 'frame_extraction_results.csv'),
                       embeddings_path=os.path.join(output_dir, 'frame_embeddings.json'),
                       clusters_path=os.path.join(output_dir, 'frame_cluster_results.csv'),
@@ -71,6 +74,7 @@ if do_cluster_embeddings:
                       id_col=id_col)
 
 if do_run_analysis:
+    from find_suspicious_frame_clusters import run_analysis
     run_analysis(cluster_label_loc=os.path.join(output_dir, 'frame_cluster_results.csv'), 
                  flags_loc=data_path, 
                  author_id_loc=data_path, 
@@ -81,6 +85,7 @@ if do_run_analysis:
                  device_num=device_num)
 
 if do_bayesian_clustering:
+    from find_suspicious_account_clusters import bayesian_clustering
     bayesian_clustering(needle_var, 
                         authors_path=os.path.join(output_dir, 'flags_and_num_posts_per_author.csv'), 
                         successes_path=os.path.join(output_dir, 'frame_cluster_use_per_author.csv'), 
@@ -96,13 +101,14 @@ if do_bayesian_clustering:
                         cholesky_rank=cholesky_rank)
 
 if do_visualize_frame_cluster_across_time:
+    from visualize_frame_clusters_across_time import visualize_frame_cluster_across_time
     visualize_frame_cluster_across_time(frame_cluster_results_loc=os.path.join(output_dir, 'frame_cluster_results.csv'),
                  original_data_loc=data_path,
                  frame_cluster_embeddings_loc=os.path.join(output_dir, 'frame_embeddings.json'),
-                 num_bins=15,
-                 round_to_nearest='D',
+                 num_bins=num_bins,
+                 round_to_nearest=round_to_nearest,
                  time_col=time_col,
-                 id_col='id',
+                 id_col=id_col,
                  num_fcs_to_display=8,
                  figures_output_loc=os.path.join(output_dir,'frame_cluster_activity_across_time.html'),
                  username=username,
