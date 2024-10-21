7ebfe7406f82f2d6e4c1af1ddff7a02578bf93e3
Carl Ehrett <cehrett@gmail.com>
2024-10-21 15:15:06 -0400

    Updated requirements.txt

--- setup.py
+++ setup.py
@@ -11,27 +11,26 @@ setup(
     author_email='cehrett@clemson.edu',
     url='https://github.com/cehrett/social_media_frame_analysis',
     install_requires=[
-        'pandas',
-        'numpy',
-        'openai',  
-        'markdown',  # Python-Markdown library
-        'IPython',  # For IPython.display functionalities
-        'torch',  # PyTorch
-        'pyro-ppl',  # Pyro probabilistic programming language
-        'matplotlib',
-        'seaborn',  # For statistical data visualization
-        'plotly',  # For interactive plots
-        'scikit-learn',  # Scikit-learn for machine learning
-        'hdbscan',  # For clustering
-        'tqdm',  # For progress bars
-        'langchain',
-        'langchain_community',
-        'langchain_core',
-        'langchain-openai',
+        'hdbscan==0.8.33',
+        'ipython==8.16.1',
+        'langchain==0.3.4',
+        'langchain_community==0.3.3',
+        'langchain_core==0.3.12',
+        'langchain-openai==0.2.3',
+        'Markdown==3.6',
+        'matplotlib==3.8.3',
+        'openai==1.52',
+        'pandas==2.1.1',
+        'plotly==5.19.0',
+        'pyro-ppl==1.8.6',
+        'scikit-learn==1.4.1.post1',
+        'seaborn==0.13.0',
+        'torch==2.4.0',
+        'tqdm==4.66.2',
         'tiktoken',
         'umap-learn',  # Uniform Manifold Approximation and Projection for Dimension Reduction
     ],
-    python_requires='>=3.9.18',
+    python_requires='>=3.11',
     include_package_data=True,
     package_data={
         'frame_extraction': ['utils/oai_system_message_template.txt']
